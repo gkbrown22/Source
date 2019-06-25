ebee6aa95d45a3d8df97da4e06f82cfa5c9ba1fe
Jonathan Berner <jonberner@gmail.com>
2019-06-25 12:08:42 +0200

    first commit: styleiqmobile _dev
    Xcode Basic Setup:
    
    - include Podfiles for Google Firebase
    - include .gitignore for Xcode

---
+++ StyleIQMobile/StyleIQMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StyleIQMobile
+//
+//  Created by Jonahan Berner on 25.06.19.
+//  Copyright © 2019 Jonahan Berner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
